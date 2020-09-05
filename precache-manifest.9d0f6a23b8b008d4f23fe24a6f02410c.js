9131a80219714ef3d3a30339ead97b1c74d2b751
Kirill Makarets <k.makarets@promodo.com>
2020-09-05 11:50:29 +0300

    add games project

---
+++ precache-manifest.9d0f6a23b8b008d4f23fe24a6f02410c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fcd1f4e9850eae315393fa397d940c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90b95d9dd82cd5b01ba",
+    "url": "/static/css/main.26f036d7.chunk.css"
+  },
+  {
+    "revision": "f2e10c8e8dab2c5f3017",
+    "url": "/static/js/2.6cf1f0de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6cf1f0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90b95d9dd82cd5b01ba",
+    "url": "/static/js/main.576c149b.chunk.js"
+  },
+  {
+    "revision": "7dffc8049defe1a21cbb",
+    "url": "/static/js/runtime-main.ac52202b.js"
+  }
+]);
