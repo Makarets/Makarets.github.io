2752d7d5859b09fdb798057bec89df0eccaf10c1
Kirill Makarets <k.makarets@promodo.com>
2020-09-05 11:48:22 +0300

    remove old project

--- IntlTelInput/examples/js/onlyCountriesEurope.js
+++
@@ -1,6 +0,0 @@
-$("#phone").intlTelInput({
-    defaultCountry: 'de',
-    preferredCountries: [ "de", "fr", "nl", "at", "ch", "be", "it" ],
-    autoPlaceholder: true,
-    utilsScript: "../../build/js/utils.min.js" // just for formatting/placeholders etc
-});
--- IntlTelInput/grunt/clean.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(grunt) {
-    return ["src/img/flags"];
-};
